81a3c377473470268d6c34de99b42267848791ca
alsolens <161577895+alsolens@users.noreply.github.com>
2025-02-26 17:41:40 +0530

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.post('/', (req, res) => {
+   res.send("hi form before")
+});
+
+app.listen(5000, () => console.log('Webhook server running on port 5000'));
